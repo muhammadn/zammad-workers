aab7d75723667efde82c4b1d19d08968522c1b1c
Muhammad Nuzaihan <muhammad@taqisystems.com>
2018-06-20 21:14:23 +0800

    focus on transactional jobs

--- main.go
+++ main.go
@@ -10,14 +10,9 @@ func main() {
         "process": "worker1",
         "server": "localhost:6379",
     })
-    workers.Process("communicate_email", CommunicateEmail, 10)
     workers.Process("default", Default, 10)
     workers.Process("transactions", Transactions, 10)
     workers.Run()
-}
-
-func CommunicateEmail(msg *workers.Msg) {
-    log.Println("running communicate email task", msg)
 }
 
 func Default(msg *workers.Msg) {
